9a8c699d39ccb7ea2fd38e87ab4f74a229be38a3
Jose Cerqueira <jose@origensmedia.com>
2019-08-08 05:46:47 +0100

    created an abstract class for sorter

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
-// const numbersCollection = new NumbersCollection([30, 3, -5, 0]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
-// console.log(numbersCollection.data);
+const numbersCollection = new NumbersCollection([30, 3, -5, 0]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection("Jose");
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
-console.log(sorter.collection);
+charactersCollection.sort();
+console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+linkedList.sort();
+linkedList.print();
